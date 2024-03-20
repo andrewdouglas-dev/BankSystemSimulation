3ddb6bfff1df469a2564eae2386a50634387d93a
ardouglas1 <andrew.r.douglas@outlook.com>
2024-03-19 17:02:49 -0700

    Added process to insert transaction records into the Transaction DB to create an Audit Trail as part of deposit and withdrawal.

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,6 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
     private String url = "jdbc:mysql://localhost:3306/Bank";
@@ -10,26 +10,36 @@ public class Transaction {
     public Transaction() {
     }
 
-    public void Deposit(String CustomerID, String AccountID, float amount) {
+    public void Deposit(String CustomerID, String accountID, float amount) {
         Account acc = new Account();
 
-        if (!acc.accountExists(CustomerID, AccountID)) {
+        if (!acc.accountExists(CustomerID, accountID)) {
             System.out.println("No Account found with provided Customer ID and Account ID");
             return;
         }
 
+        DateTimeFormatter formatterLocalDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dateTime = formatterLocalDateTime.format(LocalDateTime.now());
+
         try {
-            float bal = acc.getAccountBalance(CustomerID, AccountID);
-
             Connection connection = DriverManager.getConnection(url, user, pass);
-
             Statement statement = connection.createStatement();
 
-            String update = "Update Bank.Accounts SET Balance = '" + (bal+amount) + "' WHERE accountID = '" + AccountID + "';";
+
+            //Add transaction to Transaction DB
+            String insert = "INSERT INTO Bank.Transactions (`accountID`, `amount`, `Date`, `type`) VALUES ('" + accountID + "', '" + amount + "', '" + dateTime + "', 'Deposit');";
+
+            statement.executeUpdate(insert);
+
+
+            //Update Account Balance in Accounts DB
+            float bal = acc.getAccountBalance(CustomerID, accountID);
+
+            String update = "Update Bank.Accounts SET Balance = '" + (bal+amount) + "' WHERE accountID = '" + accountID + "';";
 
             statement.executeUpdate(update);
 
-            System.out.println("Successfully deposited to Account: " + AccountID + ". New balance is: " + acc.getAccountBalance(CustomerID, AccountID));
+            System.out.println("Successfully deposited to Account: " + accountID + ". New balance is: " + acc.getAccountBalance(CustomerID, accountID));
 
         } catch (Exception e) {
             System.out.println("There was a problem with this function");
@@ -37,31 +47,44 @@ public class Transaction {
         }
     }
 
-    public int Withdrawal(String CustomerID, String AccountID, float amount) {
+    public int Withdrawal(String CustomerID, String accountID, float amount) {
         Account acc = new Account();
 
-        if (!acc.accountExists(CustomerID, AccountID)) {
+        if (!acc.accountExists(CustomerID, accountID)) {
             System.out.println("No Account found with provided Customer ID and Account ID");
             return 1;
         }
 
-        float bal = acc.getAccountBalance(CustomerID, AccountID);
+        float bal = acc.getAccountBalance(CustomerID, accountID);
 
         if (bal < amount) {
             System.out.println("Insufficient funds, transaction not processed.");
             return 2;
         }
 
+
+        DateTimeFormatter formatterLocalDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String dateTime = formatterLocalDateTime.format(LocalDateTime.now());
+
+
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
-
             Statement statement = connection.createStatement();
 
-            String update = "Update Bank.Accounts SET Balance = '" + (bal-amount) + "' WHERE accountID = '" + AccountID + "';";
+            float transactionAmount = (float)-amount;
+
+            //Add transaction to Transaction DB
+            String insert = "INSERT INTO Bank.Transactions (`accountID`, `amount`, `Date`, `type`) VALUES ('" + accountID + "', '" + transactionAmount + "', '" + dateTime + "', 'Withdrawal');";
+
+            statement.executeUpdate(insert);
+
+
+            //Update Account Balance in Accounts DB
+            String update = "Update Bank.Accounts SET Balance = '" + (bal-amount) + "' WHERE accountID = '" + accountID + "';";
 
             statement.executeUpdate(update);
 
-            System.out.println("Successfully withdrew " + amount + " from Account: " + AccountID + " balance. New balance is: " + acc.getAccountBalance(CustomerID, AccountID));
+            System.out.println("Successfully withdrew " + amount + " from Account: " + accountID + " balance. New balance is: " + acc.getAccountBalance(CustomerID, accountID));
 
             return 3;
 
